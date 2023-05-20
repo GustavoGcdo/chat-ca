a2012c293cbc5cad2a87433640f0e318eefef2e2
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2023-05-19 20:25:34 -0400

    feat[web]:  scroll to bottom when add new messages

--- web/src/components/MessageContainer.tsx
+++ web/src/components/MessageContainer.tsx
@@ -5,15 +5,22 @@ import { useMessageStore, useUserStore } from '../store/store';
 
 const MessageContainer = () => {
   const { userLogged } = useUserStore();
-  const { messages } = useMessageStore();
-  
+  const messages = useMessageStore((state) => state.messages);
+
   const signalScroll = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // useAppStore.subscribe((state) => {
-    //   console.log('opa bao', messages.length);
-    //   signalScroll.current?.lastElementChild?.scrollIntoView();
-    // });
+    const unsubscribe = useMessageStore.subscribe(() => {
+      signalScroll.current?.lastElementChild?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'end',
+      });
+    });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   const isMyMessage = (message: Message) => {
@@ -26,7 +33,7 @@ const MessageContainer = () => {
 
   return (
     <div className="p-2 my-2 rounded h-full min-h-[400px] max-h-[400px] bg-white overflow-auto">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 pb-10" ref={signalScroll}>
         {messages.map((message, index) => (
           <div
             className={`px-2 pb-3 flex flex-col py-1 rounded relative ${
@@ -47,7 +54,7 @@ const MessageContainer = () => {
             </span>
           </div>
         ))}
-        <div ref={signalScroll}></div>
+        <div className="w-full h-[10px]"></div>
       </div>
     </div>
   );
